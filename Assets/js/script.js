b474fca3dde19df2b0b53345b99b22101632d3ef
Tyler Zeman <tylerzeman1999@gmai9l.com>
2022-07-11 22:38:08 -0500

    worked today to add HTML, CSS, and JS will add LS later

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,155 @@
-var APIKEY = '06da9f16ad96c5b068a1141025edcff1';
+function addResult(){
 
-var city;
+    inputCity = document.getElementById("myInput").value;  
+    historyList = getInfo();
+    var searchCity =$("<div>") 
+    searchCity.attr('id',inputCity) 
+    searchCity.text(inputCity) 
+    searchCity.addClass("h4")
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    
+    if (historyList.includes(inputCity) === false){
+        $(".history").append(searchCity)
+    }
+    $(".subtitle").attr("style","display:inline")
+    addInfo(inputCity);
+    
+}; 
 
-fetch(queryURL);
+//add event listener to search history item
+$(".history").on('click', function(event){
+    event.preventDefault();
+    $(".subtitle").attr("style","display:inline")
+     document.getElementById("myInput").value =  event.target.id;
+    getResult(); 
+});
 
-// Remember that the query URL won't work automatically as it's written. You'll need to adjust your application to accept user input, to store in the city variable that you've created.
+//add event listner to search button
+document.getElementById("searchBtn").addEventListener("click", addResult);
+document.getElementById("searchBtn").addEventListener('click', getResult);
+
+// Weather Result
+function getResult(){   
+
+    $(".five-day").empty();
+    $(".city").empty()
+
+   inputCity = document.getElementById("myInput").value;   
+    var countryCode='US';    
+    var cityCode=inputCity;       
+    
+    var geoLon;   
+    var geoLat;
+        
+    var cityName =$("<h>")    
+    cityName.addClass("h3")  
+    var temp = $("<div>")    
+    var wind = $("<div>")    
+    var humidity = $("<div>")   
+    var uvIndex = $("<div>")  
+    var icon =$("<img>")
+    icon.addClass("icon");    
+    var dateTime = $("<div>")
+
+    $(".city").addClass("list-group")
+    $(".city").append(cityName)    
+    $(".city").append(dateTime)    
+    $(".city").append(icon)    
+    $(".city").append(temp)    
+    $(".city").append(wind)    
+    $(".city").append(humidity)    
+    $(".city").append(uvIndex)
+    
+    
+    var geoUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityCode + "," + countryCode + "&limit=5&appid=06da9f16ad96c5b068a1141025edcff1"
+        
+      fetch(geoUrl)
+    
+        .then(function (response) {
+          return response.json();
+        })
+    
+        .then(function (data) {
+          geoLon = data[0].lon;
+          geoLat = data[0].lat;
+          
+          var weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + geoLat + "&lon="+ geoLon + "&exclude=minutely,hourly,alerts&units=imperial&appid=7d1b285353ccacd5326159e04cfab063";
+            
+          fetch(weatherUrl)
+
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            // console.log(data)
+            
+            weatherIcon= data.current.weather[0].icon;
+            imgSrc = "https://openweathermap.org/img/wn/" + weatherIcon + ".png";
+            icon.attr('src',imgSrc)
+        
+            cityName.text(cityCode);
+            var date = new Date(data.current.dt * 1000);
+            dateTime.text("("+ (date.getMonth()+1) + "/" + date.getDate() + "/" + date.getFullYear() + ")");
+
+            temp.text("Temperature: "+ data.current.temp + " F");
+            humidity.text("Humidity: " + data.current.humidity + " %");
+            wind.text("Wind Speed: " + data.current.wind_speed + " MPH"); 
+            var uvi =$("<div>")
+            uvIndex.text("UV Index: ");
+            uvi.text(data.current.uvi)
+            uvIndex.append(uvi)
+            uvIndex.addClass("d-flex")
+            
+            if (data.current.uvi < 3){
+                uvi.attr("style","background-color:green; color:black; margin-left: 5px")
+            } else if (data.current.uvi < 6){
+                uvi.attr("style","background-color:yellow; color:black; margin-left: 5px")
+            } else if (data.current.uvi < 8){
+                uvi.attr("style","background-color:orange; color:black; margin-left: 5px")
+            } else if (data.current.uvi < 11) {
+                uvi.attr("style","background-color:red; color:black; margin-left: 5px")
+            } else {
+                uvi.attr("style","background-color:purple; color:black; margin-left: 5px")
+            }
+            for (var i=1;i<6;i++){
+
+                var blueContainer = $("<div>")
+                this["futureDate"+i] = $("<h>")
+                this["futureIcon"+i] = $("<img>")
+                this["futureTemp"+i] = $("<div>")
+                this["futureWind"+i] = $("<div>")
+                this["futureHumidity"+i] = $("<div>")
+                this["forecastDay"+i] = new Date(data.daily[i].dt * 1000);     
+     
+                (this["futureDate"+i]).text(((this["forecastDay"+i]).getMonth()+1) + "/" + (this["forecastDay"+i]).getDate() + "/" + (this["forecastDay"+i]).getFullYear());
+                (this["futureTemp"+i]).text("Temperature: "+ data.daily[i].temp.day + " F");
+                (this["futureWind"+i]).text("Wind: "+ data.daily[i].wind_speed+ " MPH");
+                (this["futureHumidity"+i]).text("Humidity: " + data.daily[i].humidity + " %");
+                (this["weatherIcon"+i])= data.daily[i].weather[0].icon;
+        
+                DateimgSrc = "https://openweathermap.org/img/wn/" + (this["weatherIcon"+i]) + ".png";  
+                (this["futureIcon"+i]).attr('src',DateimgSrc)
+
+                $(".five-day").append(blueContainer)
+                blueContainer.append((this["futureDate"+i]));
+                blueContainer.append((this["futureIcon"+i]));
+                blueContainer.append((this["futureTemp"+i]));
+                blueContainer.append((this["futureWind"+i]));
+                blueContainer.append((this["futureHumidity"+i]));
+                blueContainer.addClass("weather-card")
+            }
+
+          })
+    })
+}
+
+function getInfo() {
+    var currentList =localStorage.getItem("city");
+    if (currentList !== null ){
+        freshList = JSON.parse(currentList);
+        return freshList;
+    } else {
+        freshList = [];
+    }
+    return freshList;
+}
